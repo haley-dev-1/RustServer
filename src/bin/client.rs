163acc483c78446e1942679d80dc86961819e633
Haley <hlind.engineering@gmail.com>
2025-12-19 22:45:26 -0500

    small fixes, i think i added struct within this commit

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -23,10 +23,10 @@ fn main() -> std::io::Result<()> {
 			
 		// get user input
 		let mut msg_line = String::new();
-		io::stdin().read_line(&mut msg_line);
+		io::stdin().read_line(&mut msg_line)?;
 
 		// converts input string to bytes
-		let msg = &msg_line.as_bytes().trim_ascii_end();
+		let msg = &msg_line.trim_end().as_bytes();
 		
 		// send byte array message to server, server is bound to 34000
 		sock.send_to(&msg, server)?;
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,42 @@
 
 
-use std::net::UdpSocket;
-use std::collections::HashMap;
 
-/* * UDP Server
- *   Haley Lind
+/* 
+ *   UDP Server
+ *   by Haley Lind
  *   Rust/Cyber/Networks Journey
 */
 
+use std::net::UdpSocket;
+use std::collections::HashMap;
+use std::time::Instant;
+
+
 const MAX_TRANSMISSION_UNIT: usize = 1200; // in bytes
+
+struct ClientStatistics {
+	client_name: String, // name
+	packets_sent: usize,
+	packets_received: usize, 
+	bytes_sent: usize,
+	bytes_received: usize,
+	last_seen: Instant, 
+	malformed_packets: usize,
+	oversize_packets: usize,
+	unknown_msg_types: usize
+}
+
 
 fn main() -> std::io::Result<()> {
 
-let mut client_table: HashMap<std::net::SocketAddr, usize> = HashMap::new(); // keep track of the clients connected.
-
+	let mut client_table: HashMap<std::net::SocketAddr, ClientStatistics> = HashMap::new(); // keep track of the clients connected.
 	
 	{
 		// our UDP socket
 		let sock = UdpSocket::bind("127.0.0.1:3400")?; 
 
 		// lets have a buffer of size 10 for our sock to write to
-		let mut buff = [67; MAX_TRANSMISSION_UNIT];
+		let mut buff = [0 as u8; MAX_TRANSMISSION_UNIT];
 
 		println!("\nWaiting to receive information...");
 
@@ -29,13 +45,31 @@ let mut client_table: HashMap<std::net::SocketAddr, usize> = HashMap::new(); // 
 			let (amnt, src) = sock.recv_from(&mut buff)?;
 
 			//let _: () = src;
-			let counter = client_table.entry(src).or_insert(0);
-			*counter += 1;
+			let stats = client_table.entry(src).or_insert_with(
+				|| ClientStatistics {
+					client_name: src.to_string(),
+					packets_sent: 0,
+					packets_received: 0,
+					bytes_sent: 0,
+					bytes_received: 0,
+					last_seen: Instant::now(),
+					malformed_packets: 0,
+					oversize_packets: 0,
+					unknown_msg_types: 0,
+				}
+			);
 
+			stats.client_name = "".to_string();			
+			stats.packets_received += 1;
+			stats.bytes_received += amnt;
+			stats.last_seen = Instant::now();
+			stats.packets_sent += 1;
+			
 			// TODO: Size handling on amnt e.g. if it exceeds MTU, don't receive. 
 
 			let buff = &mut buff[..amnt]; 		// grabs bytes from buffer
-			println!("CLIENT: [{src}] {:?}, size {}, count: {}", &buff, &amnt, counter);
+
+			println!("CLIENT: [{src}] {:?}, size {}, count: {}", &buff, &amnt, stats.packets_sent);
 
 			// we can do manipulation to the UDP message here e.g. reverse
 			// buff.reverse();
